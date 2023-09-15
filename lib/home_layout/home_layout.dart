0b8131dd82a035e312ced9239c13a820cb7dcb36
Sara <as1113090@gmail.com>
2023-09-15 14:21:38 +0300

    app init

---
+++ lib/home_layout/home_layout.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+  static const routeName = "Home";
+
+  const HomeLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
